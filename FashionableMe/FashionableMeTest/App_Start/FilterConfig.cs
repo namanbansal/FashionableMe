0cd22629cec37ab156750554c77c756e7dbc8892
nbansal <nbansal@visa.com>
2015-09-14 14:48:10 +0530

    +Test Project Setup +NUnit Integration

---
+++ FashionableMe/FashionableMeTest/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace FashionableMeTest
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ FashionableMe/FashionableMeTest/Controllers/HomeControllerTest.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using NUnit.Framework;
+using FashionableMe.Controllers;
+
+namespace FashionableMeTest.Controllers
+{
+    [TestFixture]
+    public class HomeControllerTest : Controller
+    {
+        [Test]
+        public void IndexTest()
+        {
+            var sut = new HomeController();
+            var result = sut.Index() as ViewResult;
+            
+            Assert.That(result.ViewBag.Message, Is.EqualTo("Modify this template to jump-start your ASP.NET MVC application."));
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your app description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}

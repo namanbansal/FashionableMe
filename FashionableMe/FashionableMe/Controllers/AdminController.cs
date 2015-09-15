93974b1557cd091efc9a7d7be4b3f6bad5c9c8fa
namanbansal <namanbansal21@gmail.com>
2015-09-15 16:32:08 +0530

    +Hashing of Password +DropDown

--- FashionableMe/FashionableMe/Controllers/AdminController.cs
+++ FashionableMe/FashionableMe/Controllers/AdminController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 
+
 namespace FashionableMe.Controllers
 {
     public class AdminController : Controller
@@ -13,14 +14,22 @@ namespace FashionableMe.Controllers
 
         public ActionResult Index()
         {
+           
+            
             return View();
         }
 
         //
         // GET: /Admin/Details/5
 
-        public ActionResult Details(int id)
+        public ActionResult Offer()
         {
+            List<SelectListItem> categoryTypes = new List<SelectListItem>();
+            categoryTypes.Add(new SelectListItem { Text = "--Select--", Value = "0", Selected = true });
+            categoryTypes.Add(new SelectListItem { Text = "Male", Value = "1" });
+            categoryTypes.Add(new SelectListItem { Text = "Female", Value = "2" });
+            categoryTypes.Add(new SelectListItem { Text = "Kids", Value = "3" });
+            ViewBag.categoryData = categoryTypes;
             return View();
         }
 
---
+++ FashionableMe/FashionableMe/Models/Offer.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.ComponentModel.DataAnnotations;
+
+namespace FashionableMe.Models
+{
+    public class Offer
+    {
+        public string OfferName { get; set; }
+        [DataType(DataType.MultilineText)]
+        public string OfferDescription { get; set; }
+        public string Category { get; set; }
+        public DateTime OfferDate { get; set; }
+        public int ApparelID { get; set; }
+        public double Discount { get; set; }
+
+        
+    }
+
+   
+}
